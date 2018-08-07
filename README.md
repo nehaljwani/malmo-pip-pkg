53d577a6e14ada26762fbcf1eec8a433e3e05bd2
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-08-07 01:32:22 -0500

    Windows: add build script

--- README.md
+++ README.md
@@ -29,3 +29,27 @@
     $ ls io/
     malmo-0.35.6.0-cp36-cp36m-macosx_10_9_x86_64.whl
     ```
+
+## Windows
+
+  - Make sure you don't have conda in `PATH` or any conda environment activated.
+    The following should throw an error:
+    ```sh
+    > where conda
+    ```
+
+  - Run the build script
+    ``` sh
+    > set MALMO_GIT_TAG=0.35.6
+    > set PYTHON_VERSION=3.5
+    > cmd /c build_malmo_wheel.bat %MALMO_GIT_TAG% %PYTHON_VERSION%
+    > dir io\
+     Volume in drive F is bigdisk
+     Volume Serial Number is E061-E588
+
+     Directory of F:\nwani\bounty\io
+
+    08/07/2018  01:10 AM    <DIR>          .
+    08/07/2018  01:10 AM    <DIR>          ..
+    08/07/2018  01:27 AM           746,091 malmo-0.35.6.0-cp35-cp35m-win_amd64.whl
+    ```
---
+++ windows/build_malmo_wheel.bat
@@ -0,0 +1,53 @@
+set GIT_TAG=%1
+set PY_VER=%2
+set "PY_VER_S=%PY_VER:.=%"
+
+set LOC=%CD%
+
+if NOT EXIST %LOC%\m3 (
+  powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://repo.anaconda.com/miniconda/Miniconda3-4.5.4-Windows-x86_64.exe', 'm3.exe') }"
+  start /wait "" m3.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /NoRegistry=1 /S /D=%CD%\m3
+  set CONDA_AUTO_UPDATE_CONDA=False
+)
+%LOC%\m3\Scripts\conda install m2w64-ntldd-git -y
+
+mkdir %LOC%\io
+
+%LOC%\m3\Scripts\conda create -yp %LOC%\py%PY_VER_S% cmake make swig python=%PY_VER% pip boost=1.67
+
+set PREFIX=%LOC%\py%PY_VER_S%
+
+rmdir /s /q malmo
+git clone https://github.com/Microsoft/malmo %LOC%\malmo
+cd malmo
+git reset --hard %GIT_TAG%
+git apply %LOC%\win.patch
+
+rmdir /s /q build
+mkdir build
+cd build
+REM Need to link zlib statically too
+del %PREFIX%\Library\lib\zlib.lib %PREFIX%\Library\lib\z.lib
+copy %PREFIX%\Library\lib\zlibstatic.lib %PREFIX%\Library\lib\zlib.lib
+copy %PREFIX%\Library\lib\zlibstatic.lib %PREFIX%\Library\lib\z.lib
+%PREFIX%\Library\bin\cmake ^
+  -G"Visual Studio 14 2015 Win64" ^
+  -DCMAKE_PREFIX_PATH=%PREFIX% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DBoost_USE_STATIC_LIBS=ON ^
+  -DUSE_PYTHON_MODULE=python%PY_VER_S% ^
+  -DUSE_PYTHON_VERSIONS=%PY_VER% ^
+  -DINCLUDE_JAVA=OFF ^
+  -DBUILD_DOCUMENTATION=OFF ^
+  -DBUILD_MOD=OFF ^
+  -DINCLUDE_CSHARP=OFF ^
+  ..
+%PREFIX%\Library\bin\cmake --build . --target INSTALL -- /p:Configuration=Release
+%LOC%\m3\Library\mingw-w64\bin\ntldd.exe install\Python_Examples\MalmoPython.pyd
+cd ..
+
+cd scripts\python-wheel
+call %LOC%\m3\Scripts\activate %LOC%\py%PY_VER_S%
+cmd /c windows_wheel.bat
+copy package\dist\*.whl %LOC%\io
+
